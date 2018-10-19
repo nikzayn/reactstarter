1b1de9d1a0124f309944cbac74c4b681894f8ce8
nikzayn <nikhilvaidyar1997@gmail.com>
2018-10-19 07:22:45 +0530

    ReactStarter
    #React Challenge 1

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            React Practice
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.github.com/nikzayn"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            Learn React
+          ><button className = "contact">
+            Contact Me</button>
           </a>
         </header>
       </div>
